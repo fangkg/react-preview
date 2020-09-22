013a09509c64013d9efe62c95159d85964b3285a
fangkg <fangkg@tfinfo.cn>
2020-09-22 15:13:40 +0800

    setState用法

---
+++ src/views/SetStatePage.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export default class SetStatePage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            count: 0
+        }
+    }
+    changeValue = v => {
+        this.setState()
+    }
+}
