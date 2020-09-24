10df73e83a0d716386036e8c29798eeeab0f3d6e
fangkg <fangkg@tfinfo.cn>
2020-09-24 09:53:11 +0800

    react-redux

--- src/index.js
+++ src/index.js
@@ -7,18 +7,19 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import logo from "./logo.svg"
 import store from "./store/index"
+import { Provider } from "react-redux";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
 
 // jsx
 // 基本使用，表达式用{}包裹
@@ -61,7 +62,16 @@ serviceWorker.unregister();
 
 
 // store中state发生变化时更新页面
-store.subscribe(() => {
-  // 重新渲染页面
-  ReactDOM.render(<App/>, document.getElementById("root"))
-})
+// store.subscribe(() => {
+//   // 重新渲染页面
+//   ReactDOM.render(<App/>, document.getElementById("root"))
+// })
+
+
+// provider提供store给子组件
+ReactDOM.render(
+  <Provider store={store}>
+    <App/>
+  </Provider>,
+  document.getElementById("root")
+)
--- src/views/ReduxPage.js
+++ src/views/ReduxPage.js
@@ -1,26 +1,51 @@
 import React, { Component } from "react";
-import store from "../store/index"
+import store from "../store/index";
+import { connect } from "react-redux";
 
-export default class ReduxPage extends Component {
-    componentDidMount() {
-        // 当state变化时通知view更新
-        store.subscribe(() => {
-            // 当前页面重新渲染
-            this.forceUpdate()
-        })
+// export default class ReduxPage extends Component {
+//     componentDidMount() {
+//         // 当state变化时通知view更新
+//         store.subscribe(() => {
+//             // 当前页面重新渲染
+//             this.forceUpdate()
+//         })
+//     }
+
+//     render() {
+//         console.log('store:', store)
+//         // dispatch 派发方法
+//         // subscribe 订阅方法
+//         // getState 获取状态
+//         return (
+//             <div>
+//                 <h3>ReduxPage</h3>
+//                 <p>{store.getState()}</p>
+//                 <button onClick={() => store.dispatch({ type: "ADD" })}>add</button>
+//             </div>
+//         )
+//     }
+// }
+
+
+export default connect(
+    // mapStateToProps 把state映射到props
+    state => ({num: state}),
+    // mapDispatchToProps 把dispatch映射到props
+    {
+        add: () =>({type: "ADD"})
     }
-
+)( class ReactReduxPage extends Component {
     render() {
-        console.log('store:', store)
-        // dispatch 派发方法
-        // subscribe 订阅方法
-        // getState 获取状态
+        // 默认把dispatch映射到props上
+        console.log('props:', this.props)
+        const { num, dispatch, add } = this.props;
         return (
             <div>
-                <h3>ReduxPage</h3>
-                <p>{store.getState()}</p>
-                <button onClick={() => store.dispatch({ type: "ADD" })}>add</button>
+                <h3>ReactReduxPage</h3>
+                <p>{num}</p>
+                {/* <button onClick={() => dispatch({type: "ADD"})}></button> */}
+                <button onClick={add}></button>
             </div>
-        )
+        );
     }
-}
+})
